f5515283a244ed7f7613debdf1899206097625d1
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-05-18 17:01:14 -0500

    Update SCCW_stratified_random_sample_splitting.R

--- R/SCCW_stratified_random_sample_splitting.R
+++ R/SCCW_stratified_random_sample_splitting.R
@@ -1,7 +1,7 @@
 ################################################################################################################################################################################ 
 # The function for randomally split 5 folds matrix (asign one as out-of-bag validation)
                                                                                       
-random_split <- function (input_matrix,marker_index, k_folds)
+random_splits <- function (input_matrix,marker_index, k_folds)
     {
     ln0<-dim(input_matrix[input_matrix[,marker_index]==0, ])[1]
     ln1<-dim(input_matrix[input_matrix[,marker_index]==1, ])[1]    
@@ -28,4 +28,28 @@ random_split <- function (input_matrix,marker_index, k_folds)
     return(input_matrix)
     }     
          
-                 
+################################################################################################################################################################################ 
+# The function for randomally split 5 folds matrix (asign one as out-of-bag validation)
+                                              
+random_split <- function (input_matrix,k_folds)
+    {
+    ln<-dim(input_matrix)[1]
+    sub<-round(ln/k_folds,0)
+    input_matrix$io<-k_folds
+    sample_list<-1:ln
+    
+    # sampling for k folds without replacement
+    for (i in 1:(k_folds-1))
+        {
+        set.seed(1)        
+        oob<-sample(sample_list, replace=F, size=sub)
+        # track the k folds
+        input_matrix[oob,'io']<-i
+        
+        # subset samples
+        ln_tmp<-sample_list[sample_list %nin% oob]
+        sample_list<-ln_tmp
+        }
+    
+    return(input_matrix)
+    }                      
