4b60d5b5a2085a7887c2aa424572e75cd33c8c8e
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-05-17 21:53:43 -0500

    Create test.sh

---
+++ Example/test.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+/usr/local/bin/Rscript /Supervised-clustering-survival/R/SCCW_supervised_clustering.R -i /Supervised-clustering-survival/Example/genomic_data_vcf.RData -d  /Supervised-clustering-survival/Example -o test_gene -k 5 
