8164c3260dbfa05aecd6673147fea7a897edcc04
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-05-17 21:57:52 -0500

    Update test.sh

--- Example/test.sh
+++ Example/test.sh
@@ -1,3 +1,3 @@
-#/bin/bash
+#!/bin/sh
 
 /usr/local/bin/Rscript /Supervised-clustering-survival/R/SCCW_supervised_clustering.R -i /Supervised-clustering-survival/Example/genomic_data_vcf.RData -d  /Supervised-clustering-survival/Example -o test_gene -k 5 
