94fe048fa28ec8f8539cc7d655facd186a32f766
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-05-19 12:15:21 -0500

    Update SCCW_combined_weighting.R

--- R/SCCW_combined_weighting.R
+++ R/SCCW_combined_weighting.R
@@ -1,7 +1,7 @@
 ################################################################################################################################################################################
 # The function for calculate weighted matrix by weight factor delta 
                                               
-matrix_weight_final <- function (matrix_weight_stats, matrix_weight_go, delta, marker_index)
+matrix_weight_finals <- function (matrix_weight_stats, matrix_weight_go, delta, marker_index)
     {
     matrix_weight_final<-matrix_weight_stats
     # tranformed matrix by weight factor delta 
@@ -9,3 +9,13 @@ matrix_weight_final <- function (matrix_weight_stats, matrix_weight_go, delta, m
     return (matrix_weight_final)
     }      
           
+################################################################################################################################################################################
+# The function for calculate weighted matrix by weight factor delta 
+                                              
+matrix_weight_final <- function (matrix_weight_stats, matrix_weight_go, delta)
+    {
+    # tranformed matrix by weight factor delta 
+    matrix_weight_final<-matrix_weight_stats*(1-delta)+matrix_weight_go*delta
+    return (matrix_weight_final)
+    }      
+                       
